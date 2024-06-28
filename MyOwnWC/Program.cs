8d7e8cc15ea89e2e68518e9b425a6223a97214e0
Kanokgan Hanmungtham <kanokgan@gmail.com>
2024-06-29 00:11:54 +0700

    Simple print string argument.

--- MyOwnWC/Program.cs
+++ MyOwnWC/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(args.Length == 0 ? "Hello World!" : $"Hello {args[0]}!");
+    }
+}
